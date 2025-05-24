afd7d3dfcc7682686b539db55054d98750c82087
Carl Schader <carl.schader@saronic.com>
2025-05-24 16:27:30 -0500

    rust

---
+++ src/bin/p1.rs
@@ -0,0 +1,8 @@
+
+const HEX: &str = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+
+fn main() {
+    println!("hex: {HEX}");
+    
+    HEX.chars
+}
